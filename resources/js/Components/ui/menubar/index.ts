bda5d92fde41759613d124e6f6581846e41eec31
Osemudiamhen Ughu <oseughu@gmail.com>
2025-03-01 13:00:06 +0000

    revert to tailwind v3

--- resources/js/Components/ui/menubar/index.ts
+++ resources/js/Components/ui/menubar/index.ts
@@ -1,5 +1,4 @@
 import { Menubar as MenubarPrimitive } from "bits-ui";
-import { cva } from 'class-variance-authority'
 import Root from "./menubar.svelte";
 import CheckboxItem from "./menubar-checkbox-item.svelte";
 import Content from "./menubar-content.svelte";
@@ -50,7 +49,3 @@ export {
 	Sub as MenubarSub,
 	RadioGroup as MenubarRadioGroup,
 };
-
-export const navigationMenuTriggerStyle = cva(
-  'group inline-flex h-10 w-max items-center justify-center rounded-md bg-background px-4 py-2 text-sm font-medium transition-colors hover:bg-accent hover:text-accent-foreground focus:bg-accent focus:text-accent-foreground focus:outline-hidden disabled:pointer-events-none disabled:opacity-50 data-active:bg-accent/50 data-[state=open]:bg-accent/50',
-)
--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,8 @@
 import { svelte } from '@sveltejs/vite-plugin-svelte';
-import tailwindcss from '@tailwindcss/vite';
+import autoprefixer from 'autoprefixer';
 import laravel from 'laravel-vite-plugin';
 import { resolve } from 'path';
+import tailwindcss from 'tailwindcss';
 import { defineConfig } from 'vite';
 
 const projectRootDir = resolve(__dirname);
@@ -9,16 +10,20 @@ const projectRootDir = resolve(__dirname);
 export default defineConfig({
     plugins: [
         laravel({
-            input: ['resources/css/app.css', 'resources/js/app.ts'],
+            input: ['resources/js/app.ts'],
             refresh: true,
         }),
         svelte(),
-        tailwindcss(),
     ],
     resolve: {
         alias: {
             '@': resolve(projectRootDir, 'resources/js'),
         },
         extensions: ['.svelte', '.ts', '.js', '.svelte.js', '.svelte.ts', '.json'],
+    },
+    css: {
+        postcss: {
+            plugins: [tailwindcss, autoprefixer],
+        },
     },
 });
